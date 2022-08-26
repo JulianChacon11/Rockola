007b3cae910b883f8eeb4801006a645e5400a536
JulianChacon11 <82472712+JulianChacon11@users.noreply.github.com>
2022-08-26 00:25:45 -0500

    Create folders
    -create folders repositories, entitites, controllers and services.
    -create index.html

---
+++ src/main/java/com/elvispresley/demo/controllers/MainController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.elvispresley.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Julian Chacon
+ */
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String cargarPaginaPrincipal(){
+        return "index";
+        }
+    }
+
