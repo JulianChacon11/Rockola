80a1c25eeb78f224b097851598c9ded7bd813b26
Andreastephgm <98714194+Andreastephgm@users.noreply.github.com>
2022-08-29 13:07:25 -0500

    cambios de clases
    Se volvió a elaborar el documento ya que por un error no se subió la información completa.

--- src/main/java/com/elvispresley/demo/entities/Administrator.java
+++ src/main/java/com/elvispresley/demo/entities/Administrator.java
@@ -4,10 +4,54 @@
  */
 package com.elvispresley.demo.entities;
 
+import java.util.Date;
+
 /**
  *
  * @author andreastefannygarciamejia
  */
-public class Administrator {
+public class Administrator extends User{
+    private String id_admin;
+
+    public Administrator(String id_admin, String name, String lastName, String email, Date date_of_birth, String phone, String gender, String nationality, boolean user_status) {
+        super(name, lastName, email, date_of_birth, phone, gender, nationality, user_status);
+        this.id_admin = id_admin;
+    }
+
+    public String getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(String id_admin) {
+        this.id_admin = id_admin;
+    }
+
+    @Override
+    public void update_data() {
+        super.update_data(); 
+    }
+
+    @Override
+    public void disable_account() {
+        super.disable_account();  
+    
+}
+
+    @Override
+    public void log_in() {
+        super.log_in();
+    }
+    
+    public void add_songs(){
+    
+    }
+    
+    public void add_gender(){
+    
+    }
+   
+    public void delete_song(){
+    
+    }
     
 }
--- src/main/java/com/elvispresley/demo/entities/Client.java
+++ src/main/java/com/elvispresley/demo/entities/Client.java
@@ -4,10 +4,48 @@
  */
 package com.elvispresley.demo.entities;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author andreastefannygarciamejia
  */
-public class Client {
+public class Client extends User{
+    
+    private String id_client;
+    private List<String>  playlist;
+
+    public Client(String id_client, List<String> playlist, String name, String lastName, String email, Date date_of_birth, String phone, String gender, String nationality, boolean user_status) {
+        super(name, lastName, email, date_of_birth, phone, gender, nationality, user_status);
+        this.id_client = id_client;
+        this.playlist = playlist;
+    }
+
+    public String getId_client() {
+        return id_client;
+    }
+
+    public void setId_client(String id_client) {
+        this.id_client = id_client;
+    }
+
+    public List<String> getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(List<String> playlist) {
+        this.playlist = playlist;
+    }
+
+    @Override
+    public void update_data() {
+        super.update_data(); 
+    }
+
+    @Override
+    public void disable_account() {
+        super.disable_account(); 
+    }
     
 }
